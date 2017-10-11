3b106d324a6d946d942e24d1a2cba1ac3cbbee8f
Nasser Tabook <nassertabook@Nassers-MacBook-Pro.local>
2017-10-11 18:49:36 +0400

    second changes

--- FCWeb/src/app/app.module.ts
+++ FCWeb/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { PostComponent } from './post/post.component';
 import { NewsComponent } from './news/news.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
 
 @NgModule({
@@ -19,12 +22,22 @@ import { HomeComponent } from './home/home.component';
     PostComponent,
     NewsComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    ServicesComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
+      {path: 'home', component: HomeComponent},
+      {path: 'about', component: AboutComponent},
+      {path: 'services', component: ServicesComponent},
+      {path: 'contact', component: ContactComponent},
       {path: 'post', component: PostComponent},
       {path: 'news', component: NewsComponent}
       ])
